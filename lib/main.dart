48fedd496905c09afda36b4ba9c78dc97697874f
DeaKusuma <deakuzuma24@gmail.com>
2025-10-22 21:33:45 +0700

    update navbar admin

--- lib/main.dart
+++ lib/main.dart
@@ -133,6 +133,18 @@ class _MainPageState extends State<MainPage> {
           onTap: _onItemTapped,
         );
         break;
+        case "admin":
+        bottomNavBar = CustomBottomNavBarAdmin(
+          selectedIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        );
+        break;
+      // case "mechanic":
+      //   bottomNavBar = CustomBottomNavBar(
+      //     selectedIndex: _selectedIndex,
+      //     onTap: _onItemTapped,
+      //   );
+      //   break;
       default:
         bottomNavBar = CustomBottomNavBar(
           selectedIndex: _selectedIndex,
